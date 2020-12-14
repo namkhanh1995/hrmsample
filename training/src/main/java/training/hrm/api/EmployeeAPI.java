d81cb8843ce2bd3e96fea8c7cc4835b800e8fca7
khanh <namkhanh18051995@gmail.com>
2020-12-14 17:14:59 +0700

    code

--- training/src/main/java/training/hrm/api/EmployeeAPI.java
+++ training/src/main/java/training/hrm/api/EmployeeAPI.java
@@ -2,10 +2,17 @@ package training.hrm.api;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import training.hrm.entity.Employee;
@@ -26,4 +33,30 @@ public class EmployeeAPI {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+    
+    @GetMapping("/api/code-employees")
+    public Employee getCodeEmployees() {
+        return employeeRepository.findTopByOrderByIdDesc();
+    }
+    
+    @GetMapping("/api/employees/{employees_id}")
+    public Employee getEmployees(@PathVariable("employees_id") long employees_id) {
+        return employeeRepository.findOne(employees_id);
+    }
+    
+    @PostMapping("/api/add-employees")
+    public Employee addEmployees(@Valid @RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+    
+    @PutMapping("/api/update-employees/{employees_id}")
+    public void updateEmployees(@RequestBody Employee employee, @PathVariable("employees_id") long employees_id) {
+        employeeRepository.delete(employees_id);
+    }
+  
+    @DeleteMapping("/api/delete-employees/{employees_id}")
+    public void deleteEmployees(@PathVariable("employees_id") long employees_id) {
+        employeeRepository.delete(employees_id);
+    }
+    
 }
--- training/src/main/java/training/hrm/repository/EmployeeRepository.java
+++ training/src/main/java/training/hrm/repository/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package training.hrm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import training.hrm.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	@Query("SELECT e FROM Employee e where e.employee_code=(SELECT max(e1.employee_code) FROM Employee e1)")
+	Employee findTopByOrderByIdDesc();
 
 }
